6cfa7ab72dad82d164833dc4ddc5a88f089259ba
radville <laura.radville@gmail.com>
2020-02-18 12:12:33 -0500

    create schema when app starts

--- app.py
+++ app.py
@@ -8,5 +8,6 @@ def hello_name(name):
     return "Hello " + name  
 
 # on running python app.py, run the flask app       
-if __name__ == "__main__":        
+if __name__ == "__main__":  
+    Schema()      
     app.run(debug=True)                   
