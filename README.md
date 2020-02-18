6a0bd5db6ebb009992b035495f186bb712516546
radville <laura.radville@gmail.com>
2020-02-18 12:08:23 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # to-do-app
-Python and Flask to do app
+
+A to-do app built in Python and Flask. Based on the tutorial by Bhavani Ravi, March 17, 2019, https://medium.com/bhavaniravi/build-your-1st-python-web-app-with-flask-b039d11f101c.
--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from flask import Flask
 # create an app instance
 app = Flask(__name__)             
 
-@app.route("/")                   
-def hello():                      
-    return "Hello World!"  
+@app.route("/<name>")                   
+def hello_name(name):                      
+    return "Hello " + name  
 
 # on running python app.py, run the flask app       
 if __name__ == "__main__":        
-    app.run()                   
+    app.run(debug=True)                   
