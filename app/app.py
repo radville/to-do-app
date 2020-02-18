72a8806203713002e5e8997e4f28032997b6c169
radville <laura.radville@gmail.com>
2020-02-18 12:49:38 -0500

    create todo route

--- app/app.py
+++ app/app.py
@@ -3,9 +3,9 @@ from flask import Flask
 # create an app instance
 app = Flask(__name__)             
 
-@app.route("/<name>")                   
-def hello_name(name):                      
-    return "Hello " + name  
+@app.route("/todo", method=["POST"])                   
+def create_todo():                      
+    return ToDoService().create(request.get_json())
 
 # on running python app.py, run the flask app       
 if __name__ == "__main__":  
